5084b3f46ab995f0413e551e04d2adfc0c721497
Ashraf Ali <ali4282271@gmail.com>
2023-12-15 19:15:37 +0500

    remove dark class

--- src/components/contactUs/formSection.tsx
+++ src/components/contactUs/formSection.tsx
@@ -223,7 +223,7 @@ const FormSection: FC<formSectionProps> = () => {
      </div>
      <div className="bg-white flex-col gap-2 p-5 rounded-3xl  w-full shadow-md">
      <h3 className="font-bold text-[20px] text-gray-700">Address</h3>
-     <div className="flex gap-2 pt-5 "><svg className="w-6 h-6 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 21">
+     <div className="flex gap-2 pt-5 "><svg className="w-6 h-6 text-gray-600 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 21">
     <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
       <path d="M8 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
       <path d="M13.8 12.938h-.01a7 7 0 1 0-11.465.144h-.016l.141.17c.1.128.2.252.3.372L8 20l5.13-6.248c.193-.209.373-.429.54-.66l.13-.154Z"/>
