51d54ddb5195cb4dfa53986020899aedb08c3681
Ashraf Ali <ali4282271@gmail.com>
2023-12-20 12:43:40 +0500

    complete contact us page

---
+++ src/components/contactUs/getInTouch.tsx
@@ -0,0 +1,155 @@
+import Image from "next/image";
+import img1 from "../../../public/images/aboutImages/2637112_370799-PBNAS5-71.webp";
+import img2 from "../../../public/images/aboutImages/435247898.webp";
+import img3 from "../../../public/images/aboutImages/56jhryur6.webp";
+import img4 from "../../../public/images/aboutImages/coins-paper-money-globe-white-statistic-form-background.webp";
+import img5 from "../../../public/images/aboutImages/gdf345345.webp";
+import img6 from "../../../public/images/aboutImages/website-development-links-seo-webinar-cyberspace-concept.webp";
+import React, { FC, ReactNode, MouseEvent } from "react";
+
+interface GetInTouchWithUsProps {}
+
+const GetInTouchWithUs: FC<GetInTouchWithUsProps> = () => {
+  return (
+    // <div className="bg-sky-900 flex justify-center">
+    //   <div className=" py-20 px-16 sm:px-16  max-w-7xl">
+    //     <p className="text-white font-extrabold  text-2xl md:text-4xl text-center">
+    //       Our Services
+    //     </p>
+
+    //     <div className="bg-sky-900 py-20 ">
+    //       <div className="flex pt-5 justify-center sm:px-16  flex-wrap gap-32">
+    //         <div className="flex flex-col justify-center items-center gap-5">
+    //           <Image
+    //             src={img6}
+    //             alt=""
+    //             width={200}
+    //             height={200}
+    //             className="md:w-24 md:h-24 h-32 w-32 rounded-full"
+    //           />
+    //           <p className="text-white font-bold text-lg text-center">
+    //             Web Application
+    //           </p>
+    //         </div>
+    //         <div className="flex flex-col justify-center items-center gap-5">
+    //           <Image
+    //             src={img1}
+    //             alt=""
+    //             width={200}
+    //             height={200}
+    //             className="md:w-24 md:h-24 h-32 w-32 rounded-full"
+    //           />
+    //           <p className="text-white font-bold text-lg  text-center">
+    //             Blockchain
+    //           </p>
+    //         </div>
+    //         <div className="flex flex-col justify-center items-center gap-5">
+    //           <Image
+    //             src={img3}
+    //             alt=""
+    //             width={200}
+    //             height={200}
+    //             className="md:w-24 md:h-24 h-32 w-32 rounded-full"
+    //           />
+    //           <p className="text-white font-bold text-lg text-center ">
+    //             Token development
+    //           </p>
+    //         </div>
+    //         <div className="flex flex-col justify-center items-center gap-5">
+    //           <Image
+    //             src={img5}
+    //             alt=""
+    //             width={200}
+    //             height={200}
+    //             className="md:w-24 md:h-24 h-32 w-32 rounded-full"
+    //           />
+    //           <p className="text-white font-bold text-lg text-center">DeFi</p>
+    //         </div>
+    //         <div className="flex flex-col justify-center items-center gap-5">
+    //           <Image
+    //             src={img2}
+    //             alt=""
+    //             width={200}
+    //             height={200}
+    //             className="md:w-24 md:h-24 h-32 w-32 rounded-full"
+    //           />
+    //           <p className="text-white font-bold text-lg text-center">
+    //             NFT Marketplace
+    //           </p>
+    //         </div>
+    //         <div className="flex flex-col justify-center items-center gap-5">
+    //           <Image
+    //             src={img4}
+    //             width={200}
+    //             height={200}
+    //             alt=""
+    //             className="md:w-24 md:h-24 h-32 w-32 rounded-full"
+    //           />
+    //           <p className="text-white font-bold text-lg text-center">
+    //             Currency Exchange
+    //           </p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+
+  
+    <section className="relative rounded-br-[250px] rounded-b-[250px] rounded-bl-[250px] flex justify-center mb-48 md:mb-32 px-5 md:px-10 pb-72 flex-col items-center mt-16 py-24 md:pt-32  bg-gradient-to-r from-sky-950   via-sky-700 via-30% to-sky-600 to-70%
+     bg-blend-multiply " >
+ <h4 className=" text-3xl md:text-4xl  lg:text-5xl font-bold text-white text-center">Get in Touch With Us.</h4>
+ <p className="text-white text-md py-3 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta quisquam, voluptate nam doloribus dignissimos quae.</p>
+ <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  md:px-12 max-w-7xl gap-5 pt-5 md:pt-10">
+  <div className="p-5 md:p-8 flex rounded-3xl bg-white">
+  <div className="bg-sky-950 p-5  flex justify-center items-center w-16 h-16  rounded-full">
+  <svg className="w-6 h-6  text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 21">
+    <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+      <path d="M8 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+      <path d="M13.8 12.938h-.01a7 7 0 1 0-11.465.144h-.016l.141.17c.1.128.2.252.3.372L8 20l5.13-6.248c.193-.209.373-.429.54-.66l.13-.154Z"/>
+    </g>
+  </svg>
+    </div>
+    <div className="pl-2">
+    <h2 className="text-green font-bold text-2xl  md:text-3xl ">Head Office</h2>
+    <p className="text-gray-700">Farzana Building karachi, Pakistan</p>
+    </div>
+  </div>
+  <div className="p-5 md:p-8 flex rounded-3xl bg-white">
+  <div className="bg-sky-950 p-5  flex justify-center items-center w-16 h-16  rounded-full">
+  <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m16.344 12.168-1.4-1.4a1.98 1.98 0 0 0-2.8 0l-.7.7a1.98 1.98 0 0 1-2.8 0l-2.1-2.1a1.98 1.98 0 0 1 0-2.8l.7-.7a1.981 1.981 0 0 0 0-2.8l-1.4-1.4a1.828 1.828 0 0 0-2.8 0C-.638 5.323 1.1 9.542 4.78 13.22c3.68 3.678 7.9 5.418 11.564 1.752a1.828 1.828 0 0 0 0-2.804Z"/>
+  </svg>
+    </div>
+    <div className="pl-2">
+    <h2 className="text-green font-bold text-2xl  md:text-3xl ">Call Us</h2>
+    <p className="text-gray-700">+1(203)-902-1401</p>
+    </div>
+  </div>
+  <div className="p-5 md:p-8 flex rounded-3xl bg-white">
+  <div className="bg-sky-950 p-5  flex justify-center items-center w-16 h-16  rounded-full">
+  <svg className="w-6 h-6  text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 2-8.4 7.05a1 1 0 0 1-1.2 0L1 2m18 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1m18 0v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2"/>
+  </svg>
+    </div>
+    <div className="pl-2">
+    <h2 className="text-green font-bold text-2xl  md:text-3xl ">Email Us</h2>
+    <p className="text-gray-700">info@bawdicsoft.com</p>
+    </div>
+  </div>
+ </div>
+    {/* sign up form */}
+    <div className="absolute -bottom-32 md:-bottom-24  shadow-2xl flex bg-white max-w-7xl mx-5 rounded-3xl py-5 md:py-16 flex-col px-5 md:px-32 justify-center gap-2 items-center">
+   <h4 className="text-2xl  md:text-3xl font-semibold text-center">Sing Up for Our Newsletter.</h4>
+   <p className="text-gray-800 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quas!</p>
+   <form className="flex justify-center flex-wrap gap-2 items-center  md:gap-0  md:w-[700px] pt-4">
+    <input  type="email" className="bg-sky-100 focus:outline-none focus:ring focus:border-sky-950  rounded-3xl p-4 py-4 md:basis-1/2" placeholder="Enter Your Email"/>
+    <button type="submit" className="bg-sky-950 px-16 hover:bg-sky-700 py-4 md:basis-1/4 text-white font-semibold ml-5 rounded-3xl">Subscribe</button>
+   </form>
+    </div>
+    </section>
+
+    </div>
+  );
+};
+export default GetInTouchWithUs;
