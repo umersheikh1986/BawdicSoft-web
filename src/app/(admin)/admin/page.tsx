7af7b2ff2528e020cc7be3e6c0eceaef9d5d2ebd
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-09-27 16:43:00 +0500

    order page completed

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,3 +1,9 @@
+import React from "react";
+import { CiGrid41 } from "react-icons/ci";
+import { FaRegHandshake } from "react-icons/fa6";
+import { IoFlashOutline } from "react-icons/io5";
+import { SlSupport } from "react-icons/sl";
+
 export default function Home() {
   return (
     <>
@@ -43,7 +49,7 @@ export default function Home() {
                 <input
                   type="text"
                   placeholder="Search for services..."
-                  className="w-full p-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full p-4 rounded-lg border text-black border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <button
                   className="absolute right-2 top-2 bg-gradient-to-r from-sky-950 px-8  via-sky-700 via-30% to-sky-600 to-70%
@@ -167,12 +173,141 @@ export default function Home() {
               {/* <p className="text-center mb-4">This is the content of card 1.</p> */}
               <div className="bg-white border-2 rounded-lg border-gray-700">
                 <img
-                  src="https://i.pinimg.com/originals/25/ac/28/25ac28d0d4e297348c00ccc5285339ed.png"
+                  src="/images/tech.png"
                   alt="Dummy Image"
                   className="w-40 h-40 rounded-lg"
                 />
               </div>
             </div>
+          </div>
+        </div>
+        <hr
+          style={{
+            border: "2",
+            height: "2px",
+            background: "skyblue",
+            margin: "20px 0",
+          }}
+        />
+        <div className="">
+          <h2 className="text-3xl p-6 mx-auto ">
+            {" "}
+            Cutting-Egde Software Talent At Your Finger Print{" "}
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-4 gap-6 p-6">
+            <div className="bg-white border-2 border-sky-700 shadow-md rounded-lg p-6">
+              <h3 className="text-lg font-semibold mb-4">
+                <CiGrid41 className="w-12 h-12" />
+              </h3>
+              <p className="text-gray-600">
+                Your One-Stop Shop: 10 Essential Categories
+              </p>
+              <p className="mt-6">
+                Get Results from Skilled freelancers from all over the world,
+                for any task , At any Price point{" "}
+              </p>
+            </div>
+            <div className="bg-white  border-2 border-sky-700 shadow-md rounded-lg p-5">
+              <h3 className="text-lg font-semibold mb-4">
+                <FaRegHandshake className="w-14 h-14" />
+              </h3>
+              <p className="text-xl text-gray-600">
+                Tranparent Pricing structure
+              </p>
+
+              <p className="mt-10">
+                {" "}
+                Pay per project or by the hour (Pro). Payment Only get released
+                When You approve{" "}
+              </p>
+            </div>
+            <div className="bg-white  border-2 border-sky-700 shadow-md rounded-lg p-6">
+              <h3 className="text-lg font-semibold mb-4">
+                <IoFlashOutline className="w-12 h-12" />
+              </h3>
+              <p className="text-gray-600 text-xl">
+                Fast-Tracked Quality Solution
+              </p>
+              <p className="mt-12">
+                Filter to find the right freelancers quickly and get great work
+                delivered in no time , every time
+              </p>
+            </div>
+            <div className="bg-white  border-2 border-sky-700 shadow-md rounded-lg p-6">
+              <h3 className="text-lg font-semibold mb-4">
+                <SlSupport className="w-12 h-12" />
+              </h3>
+              <p className="text-gray-600 text-xl">24/7 customer support.</p>
+              <p className="mt-12">
+                Chat With Our Team To Get Your Questions Answered Or Resolve Any
+                Issues With Your Orders.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <hr
+          style={{
+            border: "2",
+            height: "2px",
+            background: "skyblue",
+            margin: "20px 0",
+          }}
+        />
+        <div className="relative bg-blue-500 p-6 text-white  mx-auto  h-[250px]  max-w-7xl rounded-lg shadow-lg">
+          <h1 className="text-5xl text-center font-bold mb-4">
+            Seamless Access to Software Expertise!…
+          </h1>
+          <div className="absolute top-1/2 left-1/2  border-2  border-white inline-block text-center p-4">
+            <p className="text-4xl">Join </p>
+          </div>
+        </div>
+        {/* Footer  */}
+        <hr
+          style={{
+            border: "2",
+            height: "2px",
+            background: "skyblue",
+            margin: "20px 0",
+          }}
+        />
+        <div className="grid grid-cols-1 sm:grid-cols-2 h-[250px] lg:grid-cols-5  p-6">
+          <div className="bg-blue-500 text-white p-4  ">
+            <p className="text-xl  text-center">Categories</p>
+            <ul className="mt-6 gap-3">
+              <li className="text-center">Graphics & Design</li>
+              <li className="mt-2 text-center">Digital Marketing</li>
+            </ul>
+          </div>
+          <div className="bg-blue-500 text-white p-4  ">
+            <p className="text-xl text-center">For Clients</p>
+            <ul className="mt-6">
+              <li className="text-center">How Fiver Works</li>
+              <li className="mt-2 text-center">Customer Success Stories</li>
+            </ul>
+          </div>
+          <div className="bg-blue-500 text-white p-4  ">
+            <p className="text-center">For Freelancers</p>
+            <ul className="mt-6">
+              <li className="text-center">Become A Fiver Freelancer</li>
+              <li className="mt-2  text-center">Customer Success Stories</li>
+            </ul>
+          </div>
+          <div className="bg-blue-500 text-white p-4  ">
+            <p className="text-center">Bussiness Solutions</p>
+            <ul className="mt-6">
+              <li className="text-center">Fiver Pro</li>
+              <li className="mt-2  text-center">
+                Project Management Services{" "}
+              </li>
+            </ul>
+          </div>
+          <div className="bg-blue-500 text-white p-4  ">
+            <p className="text-center">Company</p>
+            <ul className="mt-6">
+              <li className="text-center">About Fiver</li>
+              <li className="mt-2  text-center">Help and Support </li>
+            </ul>
           </div>
         </div>
       </div>
